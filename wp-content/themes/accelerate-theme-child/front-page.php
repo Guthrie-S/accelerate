4f3552353d0c8754c0e64fb2786faf20a5ba0709
Guthrie.S <39205001+Guthrie-S@users.noreply.github.com>
2018-05-31 18:02:59 -0700

    wp pre-check

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -7,6 +7,7 @@
  * and that other 'pages' on your WordPress site will use a
  * different template.
  *
+ *
  * @package WordPress
  * @subpackage Accelerate Marketing
  * @since Accelerate Marketing 2.0
