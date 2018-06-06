11d2769e0991b64d00c47185ef86d0f1f234f051
Guthrie.S <39205001+Guthrie-S@users.noreply.github.com>
2018-06-05 21:23:53 -0700

    finished case studies

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,15 +14,56 @@
 
 get_header(); ?>
 
-    <div id="primary" class="site-content">
-        <div class="main-content" role="main">
-            <?php while ( have_posts() ) : the_post(); ?>
-            <article class="case-study">    
-                <?php the_content(); ?>
-            </article>    
+<div id="primary" class="site-content">
+    <div class="main-content" role="main">
+
+    <?php while ( have_posts() ) : the_post(); 
+        $size="full";
+        $services = get_field('services');
+        $client = get_field('client');
+        $link = get_field('site_link');
+        $image_1 = get_field('image_1');
+        $image_2 = get_field('image_2');
+        $image_3 = get_field('image_3'); ?>
+
+
+    <article class="case-study">    
+        <aside class="case-study-sidebar">
+        <h2><?php the_title(); ?></h2>
+        <h5><?php echo $services; ?></h5>
+        <h6>Client: <?php echo $client; ?></h6>
+
+        <?php the_content(); ?>
+
+        <p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
+
+        </aside>
+
+        <div class="case-study-images">
+            <?php if($image_1){ 
+                echo wp_get_attachment_image( $image_1, $size );
+                 } ?>    
+            <?php if($image_2){ 
+                echo wp_get_attachment_image( $image_2, $size );
+                 } ?> 
+            <?php if($image_3){ 
+                echo wp_get_attachment_image( $image_3, $size );
+                 } ?>        
+        </div>
+
+        
+        </article>    
+    
             <?php endwhile; // end of the loop. ?>
         </div><!-- .main-content -->
 
     </div><!-- #primary -->
 
+<nav id="navigation" class="container">
+    <div class="center"><a href="<?php echo site_url('/case-studies/') ?>">&larr; <span>Back to Case Studies</span></a></div>
+</nav>
+
+
 <?php get_footer(); ?>
+
+
