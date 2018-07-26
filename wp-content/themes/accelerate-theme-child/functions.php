e55adad6bcceb31642d58ed56bd58a5eb12cc7a3
Guthrie.S <39205001+Guthrie-S@users.noreply.github.com>
2018-07-25 22:22:24 -0700

    modifying footer

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -13,6 +13,9 @@
 
 
 
+
+
+
 function accelerate_child_scripts(){
 	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
